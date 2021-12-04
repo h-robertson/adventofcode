4c823cc2cc4f4ad03c7aaa943ee949be4bc1727a
Helena <h.robertson@icloud.com>
2021-12-04 02:04:09 +0000

    day 3 part 2

--- day3/index.js
+++ day3/index.js
@@ -1,5 +1,10 @@
 // Day 3 - Part 1
 
+function binToDec(binaryValue) {
+    var decimal = parseInt(binaryValue, 2)
+    return decimal;
+}
+
 fetch('day3/input.txt')
     .then(response => response.text())
     .then(function (text) {
@@ -28,6 +33,43 @@ fetch('day3/input.txt')
             gamma[k] === "0" ? epsilon += "1" : epsilon += "0";
         }
 
-        $('#3-p1').append(`<p class="answer">${parseInt(gamma,2)*parseInt(epsilon,2)}</p>`)
+        $('#3-p1').append(`<p class="answer">${binToDec(gamma)*binToDec(epsilon)}</p>`)
+
+
+// Day 3 - Part 2
+
+        let most, least;
+        let most_input = least_input = input;
+
+        for (let i = 0; i < input[0].length; i++) {
+
+            if (most_input.length > 1) {
+                zero_most = 0;
+                one_most = 0;
+
+                for (let j = 0; j < most_input.length; j++) {
+                    most_input[j][i] === "0" ? zero_most ++ : one_most ++;                
+                }
+
+                zero_most > one_most ? most = "0" : most = "1";
+                most_input = most_input.filter(x => x[i] === most);
+
+            }
+
+            if (least_input.length > 1) {
+                zero_least = 0;
+                one_least = 0;
+
+                for (let j = 0; j < least_input.length; j++) {
+                    least_input[j][i] === "0" ? zero_least ++ : one_least ++;
+                }
+
+                zero_least <= one_least ? least = "0" : least = "1";
+                least_input = least_input.filter(x => x[i] === least);
+            }
+
+        }
+
+        $('#3-p2').append(`<p class="answer">${binToDec(most_input) * binToDec(least_input)}</p>`)
 
     });
